acfff921c21a586e818678065bb531100dcea741
user <l.bohlin@gmx.de>
2024-10-14 20:56:30 +0200

    implemented base prototype

--- mapping.py
+++ mapping.py
@@ -1,14 +1,5 @@
 import subprocess, sys
 from pathlib import Path
-dummypic = "/home/user/Documents/Projekte/Image/error.jpeg"
-class pic:
-    path = Path
-    def __init__(self, path):
-        if path.exists():
-            self.path = path
-        else:
-            self.path = Path(dummypic)
-        self.ppath = path.parent
 
 class obj:
     k = str
@@ -30,18 +21,16 @@ class Mapping:
                 if p.exists():
                     f = open(p, "r")
                     lines = f.readlines()
-                    print(lines)
                     for l in lines:
                         exists = False
                         for item in self.data:
                             if item.k == l:
-                                print("true")
-                                exist = True
-                                item.pics.append(pic(picpath))
+                                exists = True
+                                item.pics.append(picpath)
                                 break
                         if not exists:
                             nobj = obj(l)                        
-                            nobj.pics.append(pic(picpath))                        
+                            nobj.pics.append(picpath)
                             self.data.append(nobj)
 
     def printdata(self):
@@ -49,7 +38,7 @@ class Mapping:
         for o in self.data:            
             print(o.k)
             for p in o.pics:
-                print(p.path)
+                print(p)
 
             
                     
--- tkintertest.py
+++ tkintertest.py
@@ -1,8 +1,9 @@
 from tkinter import *
-
 from PIL import ImageTk, Image
+from pathlib import Path
 
 import mapping
+dummypic = "/home/user/Documents/Projekte/Image/error.jpeg"
 root = Tk()
 root.title("Using Pack")
 root.geometry("1000x600")  # set starting size of window
@@ -14,14 +15,48 @@ pictureFrame = Frame(root, width=200, height=400, bg='grey')
 pictureFrame.grid(row=1, column=0, padx=10, pady=5)
 #scale = Scale(master=root, orient=VERTICAL, from_=1, to=len(imageList)/8, resolution=1, showvalue=False, command=toFrame)
 #scale.grid(row=1, column=1)
+m = mapping.Mapping("/home/user/Documents/Projekte/Image/test")
+pictures = []
+labels = []
 def searchFunction():
     x = searchEntry.get()
-    print(x)
+    if not len(x) > 1:
+        return
+    global labels
+    for l in labels:        
+        l.destroy()
+    global pictures
+    pictures = []
+    imageList = []
+
+    labels = []
+    for obj in m.data:                
+        if x in obj.k:
+            for p in obj.pics:
+                imageList.append(p)    
+    r = 0
+    c = 0
+    for img in imageList:        
+        if c > 3:
+            c = 0
+            r = r + 1
+        ppath = Path(img).parent        
+        if not img.exists():
+            global dummypic
+            img = Path(dummypic)
+        temp = Image.open(img)
+        temp = temp.resize((150, 100))
+        image = ImageTk.PhotoImage(temp)                
+        pictures.append(image)                       
+        label = Label(pictureFrame, text=ppath, image=image, compound='top')
+        label.grid(row=r, column=c, padx=1, pady=1)
+        labels.append(label)
+        c = c + 1
+    pictureFrame.update()
+
 
 
 searchFrame.grid(row=0,column=0)
-
-#Label(searchFrame, bg="#6FAFE7").pack(side='left', padx=5)
 
 searchEntry = Entry(searchFrame, bd=3)
 searchEntry.grid(row=0, column=0)
@@ -29,29 +64,9 @@ searchEntry.grid(row=0, column=0)
 searchbutton = Button(root, text="search", command=searchFunction)
 searchbutton.grid(row=0, column=1)
 
-def toFrame(i):
-    print("next")
 
-m = mapping.Mapping("/home/user/Documents/Projekte/Image/test")
 m.printdata()
 
-imageList = ["pic/pic.jpg","pic/pic2.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif", "pic/pic.gif","pic/pic2.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif","pic/pic.gif"]
-
-r = 0
-c = 0
-pictures = []
-for img in imageList:
-    if c > 3:
-        c = 0
-        r = r + 1
-    temp = Image.open(img)
-    temp=temp.resize((150, 100))
-    image = ImageTk.PhotoImage(temp)
-    
-    #original_image = image.subsample(5,5)  # resize image using subsample
-    pictures.append(image)
-    Label(pictureFrame, image=image).grid(row=r, column=c, padx=1, pady=1)
-    c = c + 1
 
 
 root.mainloop()
